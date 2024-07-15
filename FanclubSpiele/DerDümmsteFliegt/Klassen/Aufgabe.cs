f68128d583daebf999c32a550462823765d5e78d
2L0sT4u <dennis.duergerow@gmail.com>
2024-07-15 23:36:36 +0200

    feat: Klassen -> Spieler, Aufgabe

---
+++ FanclubSpiele/DerDümmsteFliegt/Klassen/Aufgabe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FanclubSpiele.DerDümmsteFliegt.Klassen
+{
+    internal class Aufgabe
+    {
+        private int id {  get; set; }
+        private string frage { get; set; }
+        private string antwort { get; set; }
+    }
+}
---
+++ FanclubSpiele/DerDümmsteFliegt/Klassen/Spieler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FanclubSpiele.DerDümmsteFliegt.Klassen
+{
+    internal class Spieler
+    {
+        private int id { get; set; }
+        private string name { get; set; }
+        private string leben { get; set; }
+
+        public Spieler(int id, string name, string leben)
+        {
+            this.id = id;
+            this.name = name;
+            this.leben = leben;
+        }
+    }
+}
