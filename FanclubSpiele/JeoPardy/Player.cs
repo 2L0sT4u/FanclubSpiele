ffa3cad55a87133f326d26c479afff0e706d953f
Nichibotsu <81435611+Nichibotsu@users.noreply.github.com>
2024-07-15 23:09:44 +0200

    .

---
+++ FanclubSpiele/JeoPardy/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FanclubSpiele.JeoPardy
+{
+    internal class Player
+    {
+        public Player() {
+            string Name = string.Empty;
+            int Farbe = 0;
+        }
+
+
+        
+
+    }
+}
---
+++ FanclubSpiele/JeoPardy/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FanclubSpiele.JeoPardy
+{
+    internal class Team
+    {
+        public Team(Player p1_ref ,Player p2_ref)
+        {
+            Player p1 = p1_ref;
+            Player p2 = p2_ref;
+        }   
+
+
+    }
+}
