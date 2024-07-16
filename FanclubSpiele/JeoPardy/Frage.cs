dff6b922980c4d4b59ff78c4b61b10a8e9da01ba
Nichibotsu <81435611+Nichibotsu@users.noreply.github.com>
2024-07-16 13:39:15 +0200

    Frage

---
+++ FanclubSpiele/JeoPardy/Frage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FanclubSpiele.Jeopardy
+{
+    internal class Frage
+    {
+        private string frage;
+        private string antwort;
+        private int reward;
+
+        public Frage(string Frage,string Antwort,int reward) {
+            this.frage = Frage;
+            this.antwort = Antwort;
+            this.reward = reward;
+        }
+
+    }
+}
