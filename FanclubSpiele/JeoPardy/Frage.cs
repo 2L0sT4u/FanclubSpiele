fd7d814fa312abc5ced320a99c41dc91edce1826
Nichibotsu <81435611+Nichibotsu@users.noreply.github.com>
2024-07-16 13:47:21 +0200

    Frage get & set

--- FanclubSpiele/JeoPardy/Frage.cs
+++ FanclubSpiele/JeoPardy/Frage.cs
@@ -11,19 +11,24 @@ namespace FanclubSpiele.Jeopardy
         private string frage;
         private string antwort;
         private int reward;
+        private string group;
 
-        public Frage(string Frage,string Antwort,int reward) {
+        public Frage(string Frage,string Antwort,string Group,int reward) {
             this.frage = Frage;
             this.antwort = Antwort;
             this.reward = reward;
+            this.group = Group;
         }
 
         public string getFrage() { return this.frage; }
         public string getAntwort() { return this.antwort; }
+        public string getGroup() {  return this.group; }
         public int getReward() { return this.reward; }
+
         public void setFrage(string x) { this.frage = x; }
         public void setAntwort(string x) { this.antwort = x; }
         public void setReward(int x) { this.reward = x; }
+        public void setGroup(string x) {this.group = x; }
         
     }
 }   
